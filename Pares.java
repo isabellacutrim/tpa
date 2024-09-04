e2ae9d4526e210b353be167767e38b6455085080
Isabella Cutrim <165211676+isabellacutrim@users.noreply.github.com>
2024-09-04 14:06:38 -0300

    Add files via upload

---
+++ Pares.java
@@ -0,0 +1,10 @@
+package estuturaWhile4exerc;
+public class Pares {
+	public static void main(String[] args) {
+		int i = 2;
+		while (i <201) {
+			System.out.println(i);
+			i +=2;
+	}
+  }
+}
---
+++ Somatoria.java
@@ -0,0 +1,13 @@
+package estuturaWhile4exerc;
+public class Somatoria {
+    public static void main(String[] args) {
+    int i=1,soma=0;
+    while(i<=100){
+       soma+=i;
+       i++;
+       }
+    System.out.println("o resultado é: "+soma);
+    }
+}
+
+
